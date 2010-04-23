db52e86fbacd73935995b528a7febd6cbe3bb332
Sil Westerveld <sil.westerveld@sara.nl>
2010-04-23 10:57:49 +0000

    Fixed deprecation warning: 	admin.site.root is deprecated

--- sara_cmt/sara_cmt/urls.py
+++ sara_cmt/sara_cmt/urls.py
@@ -14,7 +14,8 @@ urlpatterns = patterns('',
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    (r'^admin/(.*)', admin.site.root),
+    #(r'^admin/(.*)', admin.site.root),
+    (r'^admin/(.*)', include(admin.site.urls)),
     (r'^db/(.*)', databrowse.site.root),
 
     # !!! Multiple admin-sites !!!
