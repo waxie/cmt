cf348c16800e537f0c98d727572223994c36d504
Ramon Bastiaans <ramon.bastiaans@surfsara.nl>
2013-04-08 14:07:30 +0200

    setup.py:
     * reset dependancies to non-rest api release for 1.1.0

--- sara_cmt/setup.py
+++ sara_cmt/setup.py
@@ -50,9 +50,8 @@ setup(
     packages = ['sara_cmt', 'sara_cmt.cluster', 'sara_cmt.cluster.templatetags'],
 
     install_requires = [
-        'djangorestframework',
         'Python>=2.6',
-        'Django>=1.3',
+        'Django>=1.2, <1.3',
         'IPy>=0.75',
         'django_extensions>=0.4',
         'django_tagging>=0.3.1',
