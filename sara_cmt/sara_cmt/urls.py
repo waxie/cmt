2ad785e9c1393f5c2c8d5d90071b59ec3479eecc
Sil Westerveld <sil.westerveld@surfsara.nl>
2013-08-13 17:38:46 +0200

    Modifications to get CMT working with Django 1.5, which is needed to implement the Django REST framework (ticket #29).

--- sara_cmt/sara_cmt/urls.py
+++ sara_cmt/sara_cmt/urls.py
@@ -15,8 +15,7 @@
 #    along with this program; if not, write to the Free Software
 #    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-from django.conf.urls.defaults import include, patterns, url
-from django.contrib import databrowse
+from django.conf.urls import include, patterns, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -31,42 +30,8 @@ urlpatterns = patterns('',
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    (r'^admin/', include(admin.site.urls)),
-    (r'^db/(.*)', databrowse.site.root),
+    url(r'^admin/', include(admin.site.urls)),
 
     # !!! Multiple admin-sites !!!
     #  http://docs.djangoproject.com/en/dev/ref/contrib/admin/#multiple-admin-sites-in-the-same-urlconf
 )
-
-# core
-from sara_cmt.cluster.models import Cluster, HardwareUnit, Interface, \
-                                    Network, Rack
-databrowse.site.register(Cluster)
-databrowse.site.register(HardwareUnit)
-databrowse.site.register(Interface)
-databrowse.site.register(Network)
-databrowse.site.register(Rack)
-# locations
-from sara_cmt.cluster.models import Country, Address, Room
-databrowse.site.register(Country)
-databrowse.site.register(Address)
-databrowse.site.register(Room)
-# contacts
-from sara_cmt.cluster.models import Company, Telephonenumber, Connection
-databrowse.site.register(Company)
-databrowse.site.register(Telephonenumber)
-databrowse.site.register(Connection)
-# specifications
-from sara_cmt.cluster.models import HardwareModel, Role, InterfaceType
-databrowse.site.register(HardwareModel)
-databrowse.site.register(Role)
-databrowse.site.register(InterfaceType)
-# support
-from sara_cmt.cluster.models import WarrantyContract, WarrantyType
-databrowse.site.register(WarrantyContract)
-databrowse.site.register(WarrantyType)
-
-# tags
-from tagging.models import *
-databrowse.site.register(Tag)
-databrowse.site.register(TaggedItem)
