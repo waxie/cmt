f66e302a7f43dec616860f4a8d115071820fe6ed
Sil Westerveld <sil.westerveld@surfsara.nl>
2013-09-04 15:28:10 +0200

    Made views and serializers for all models, and defined urls to make them accessible through the API.

--- sara_cmt/cmt_server/apps/api/serializers.py
+++ sara_cmt/cmt_server/apps/api/serializers.py
@@ -1,35 +1,128 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
-from sara_cmt.cluster.models import HardwareUnit, Cluster, Rack
+from sara_cmt.cluster.models import Cluster, Interface, Network, Rack 
+from sara_cmt.cluster.models import HardwareUnit as Equipment
+from sara_cmt.cluster.models import Country, Address, Room
+from sara_cmt.cluster.models import Company, Connection, Telephonenumber
+from sara_cmt.cluster.models import HardwareModel, Role, InterfaceType
+from sara_cmt.cluster.models import WarrantyType, WarrantyContract
+
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'email', 'groups')
+        fields = ('url', 'username', 'email', 'groups')
 
 
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
-        fields = ('name',)
+        fields = ('url', 'name',)
+
+
+#####
+#
+# Serializers based on the models in CMT
+#
+
+
+#class ClusterSerializer(serializers.ModelSerializer):
+class ClusterSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Cluster
+        fields = ('url', 'name', 'machinenames', 'hardware')
 
 
 #class HardwareSerializer(serializers.ModelSerializer):
-class HardwareSerializer(serializers.HyperlinkedModelSerializer):
+class EquipmentSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = HardwareUnit
-        #fields = ('rack', 'first_slot', 'label', 'warranty_tag')
-        fields = ('url', 'rack', 'first_slot', 'label', 'warranty_tag')
+        model = Equipment
+        #fields = ('url', 'rack', 'first_slot', 'label', 'warranty_tag')
+        fields = ('url', 'cluster', 'role', 'network', 'specifications', 'warranty', 'rack', 'seller', 'owner', 'state', 'warranty_tag', 'serial_number', 'first_slot', 'label')
 
 
-class ClusterSerializer(serializers.ModelSerializer):
+class InterfaceSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = Cluster
-        fields = ('name',)
+        model = Interface
+        fields = ('url', 'network', 'host', 'iftype', 'label', 'aliases', 'hwaddress', 'ip')
 
 
-class RackSerializer(serializers.ModelSerializer):
+class NetworkSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Network
+        #fields = ('url', 'name', 'netaddress', 'netmask', 'gateway', 'domain', 'vlan', 'hostnames', 'hardware', 'interfaces')
+        fields = ('url', 'name', 'gateway', 'domain', 'vlan', 'hostnames', 'hardware', 'interfaces')
+
+
+#class RackSerializer(serializers.ModelSerializer):
+class RackSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Rack
-        fields = ('label',)
+        fields = ('url', 'room', 'label', 'capacity', 'contents')
+
+
+class CountrySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Country
+        fields = ('url', 'name', 'country_code', 'addresses')
+
+
+class AddressSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Address
+        fields = ('url', 'country', 'address', 'postalcode', 'city')
+
+
+class RoomSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('url', 'address', 'floor', 'label', 'racks')
+
+
+class CompanySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Company
+        fields = ('url', 'addresses', 'name', 'website')
+
+
+class ConnectionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Connection
+        fields = ('url', 'address', 'company', 'active', 'name', 'email', 'sold', 'owns')
+
+
+class TelephonenumberSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Telephonenumber
+        fields = ('url', 'country', 'connection', 'areacode', 'subscriber_number', 'number_type')
+
+
+class HardwareModelSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = HardwareModel
+        fields = ('url', 'vendor', 'name', 'vendorcode', 'rackspace', 'expansions', 'hardware')
+
+
+class RoleSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Role
+        fields = ('url', 'label', 'hardware')
+
+
+class InterfaceTypeSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = InterfaceType
+        fields = ('url', 'vendor', 'label')
+
+
+class WarrantyTypeSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = WarrantyType
+        fields = ('url', 'contact', 'label')
+
+
+class WarrantyContractSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = WarrantyContract
+        fields = ('url', 'warranty_type', 'contract_number', 'annual_cost', 'label', 'date_from', 'date_to', 'hardware')
--- sara_cmt/cmt_server/urls.py
+++ sara_cmt/cmt_server/urls.py
@@ -3,12 +3,29 @@ from django.contrib.auth.models import User, Group
 from rest_framework import viewsets, routers
 
 from django.core.urlresolvers import reverse
-from sara_cmt.cluster.models import HardwareUnit, Cluster, Rack
-from cmt_server.apps.api.serializers import UserSerializer, GroupSerializer, HardwareSerializer, ClusterSerializer, RackSerializer
+from sara_cmt.cluster.models import Cluster, Interface, Network, Rack 
+from sara_cmt.cluster.models import HardwareUnit as Equipment
+from sara_cmt.cluster.models import Country, Address, Room
+from sara_cmt.cluster.models import Company, Connection, Telephonenumber
+from sara_cmt.cluster.models import HardwareModel, Role, InterfaceType
+from sara_cmt.cluster.models import WarrantyType, WarrantyContract
+
+from cmt_server.apps.api.serializers import UserSerializer, GroupSerializer, ClusterSerializer, EquipmentSerializer, InterfaceSerializer, NetworkSerializer, RackSerializer
+from cmt_server.apps.api.serializers import CountrySerializer, AddressSerializer, RoomSerializer
+from cmt_server.apps.api.serializers import CompanySerializer, ConnectionSerializer, TelephonenumberSerializer
+from cmt_server.apps.api.serializers import HardwareModelSerializer, RoleSerializer, InterfaceTypeSerializer
+from cmt_server.apps.api.serializers import WarrantyTypeSerializer, WarrantyContractSerializer
 
 
 # ViewSets define the view behavior.
 class UserViewSet(viewsets.ModelViewSet):
+    """
+    Returns a list of **all** registered users.
+    
+    For more details about the user please [see here][ref].
+
+    [ref]: https://intranet.surfsara.nl/algemeen/wiki/Telefoonlijst
+    """
     model = User
     queryset = User.objects.all()
     serializer_class = UserSerializer
@@ -20,16 +37,28 @@ class GroupViewSet(viewsets.ModelViewSet):
     serializer_class = GroupSerializer
 
 
-class HardwareViewSet(viewsets.ModelViewSet):
-    model = HardwareUnit
-    queryset = HardwareUnit.objects.all()
-    serializer_class = HardwareSerializer
-
-
 class ClusterViewSet(viewsets.ModelViewSet):
     model = Cluster
     queryset = Cluster.objects.all()
     serializer_class = ClusterSerializer
+
+
+class EquipmentViewSet(viewsets.ModelViewSet):
+    model = Equipment
+    queryset = Equipment.objects.all()
+    serializer_class = EquipmentSerializer
+
+
+class InterfaceViewSet(viewsets.ModelViewSet):
+    model = Interface
+    queryset = Interface.objects.all()
+    serializer_class = InterfaceSerializer
+
+
+class NetworkViewSet(viewsets.ModelViewSet):
+    model = Network
+    queryset = Network.objects.all()
+    serializer_class = NetworkSerializer
 
 
 class RackViewSet(viewsets.ModelViewSet):
@@ -38,13 +67,92 @@ class RackViewSet(viewsets.ModelViewSet):
     serializer_class = RackSerializer
 
 
+class CountryViewSet(viewsets.ModelViewSet):
+    model = Country
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+
+
+class AddressViewSet(viewsets.ModelViewSet):
+    model = Address
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
+
+
+class RoomViewSet(viewsets.ModelViewSet):
+    model = Room
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+
+class CompanyViewSet(viewsets.ModelViewSet):
+    model = Company
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+
+
+class ConnectionViewSet(viewsets.ModelViewSet):
+    model = Connection
+    queryset = Connection.objects.all()
+    serializer_class = ConnectionSerializer
+
+
+class TelephonenumberViewSet(viewsets.ModelViewSet):
+    model = Telephonenumber
+    queryset = Telephonenumber.objects.all()
+    serializer_class = TelephonenumberSerializer
+
+
+class HardwareModelViewSet(viewsets.ModelViewSet):
+    model = HardwareModel
+    queryset = HardwareModel.objects.all()
+    serializer_class = HardwareModelSerializer
+
+
+class RoleViewSet(viewsets.ModelViewSet):
+    model = Role
+    queryset = Role.objects.all()
+    serializer_class = RoleSerializer
+
+
+class InterfaceTypeViewSet(viewsets.ModelViewSet):
+    model = InterfaceType
+    queryset = InterfaceType.objects.all()
+    serializer_class = InterfaceTypeSerializer
+
+
+class WarrantyTypeViewSet(viewsets.ModelViewSet):
+    model = WarrantyType
+    queryset = WarrantyType.objects.all()
+    serializer_class = WarrantyTypeSerializer
+
+
+class WarrantyContractViewSet(viewsets.ModelViewSet):
+    model = WarrantyContract
+    queryset = WarrantyContract.objects.all()
+    serializer_class = WarrantyContractSerializer
+
+
 # Routers provide an easy way of automatically determining the URL conf
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'groups', GroupViewSet)
-router.register(r'hardware', HardwareViewSet)
 router.register(r'clusters', ClusterViewSet)
+router.register(r'equipment', EquipmentViewSet)
+router.register(r'interfaces', InterfaceViewSet)
+router.register(r'networks', NetworkViewSet)
 router.register(r'racks', RackViewSet)
+router.register(r'countries', CountryViewSet)
+router.register(r'addresses', AddressViewSet)
+router.register(r'rooms', RoomViewSet)
+router.register(r'companies', CompanyViewSet)
+router.register(r'contacts', ConnectionViewSet)
+router.register(r'telephonenumbers', TelephonenumberViewSet)
+router.register(r'hardwaremodels', HardwareModelViewSet)
+router.register(r'roles', RoleViewSet)
+router.register(r'interfacetypes', InterfaceTypeViewSet)
+router.register(r'warrantytypes', WarrantyTypeViewSet)
+router.register(r'warrantycontracts', WarrantyContractViewSet)
 
 
 # Wire up our API using automatic URL routing.
