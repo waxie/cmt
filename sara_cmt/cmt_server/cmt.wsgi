6cb43858a8b74d513b5da978c0307eee0015b02f
Ramon Bastiaans <ramon.bastiaans@surfsara.nl>
2014-04-02 20:51:10 +0200

    cmt_server/cmt.wsgi:
     * added new, improved WSGI loader for CMT
     * now prints errors generated by settings.py on web page
       * i.e. cmt.conf errors, etc
    
    cmt_server/static/debug_toolbar,
    cmt_server/static/rest_framework:
    
     * added some more static files
    
    cmt_client/setup.py,
    cmt_server/setup.py:
    
     * first draft (untested) for packaging client and server seperately
     * see #13
    
    cmt_server/apps/cluster/migrations/:
    
     * some migrations unsure we might need

---
+++ sara_cmt/cmt_server/cmt.wsgi
@@ -0,0 +1,58 @@
+#    This file is part of CMT, a Cluster Management Tool made at SURFsara.
+#    Copyright (C) 2012, 2013  Sil Westerveld, Ramon Bastiaans
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+#
+# Add this to apache config:
+#
+#  WSGIPassAuthorization On
+#  WSGIScriptAlias / /path/to/virtualenvs/cmt/cmt.wsgi
+#  SetEnv VIRTUALENV /path/to/virtualenvs/cmt
+
+import os
+
+from exceptions import SystemExit
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cmt_server.settings")
+
+def application(environ, start_response):
+    ## Try to activate a virtualenv environment when the
+    ## VIRTUALENV environ var is set
+    virtualenv = environ.get('VIRTUALENV', False)
+
+    if virtualenv and os.path.exists(virtualenv):
+        activate_this = os.path.join(virtualenv, 'bin/activate_this.py')
+        if os.path.exists(activate_this):
+            execfile(activate_this, dict(__file__=activate_this))
+
+    err_msgs = None
+
+    try:
+        from django.core.wsgi import get_wsgi_application
+        return get_wsgi_application()(environ, start_response)
+
+    except SystemExit as details:
+
+        err_msgs = str( details )
+
+    except ImportError as details:
+
+        err_msgs = str( details )
+
+    if err_msgs:
+
+        start_response('200 OK',[('Content-type','text/html')])
+        return ['<html><body><h1>Unable to load CMT/Django environment!</h1><p>' + err_msgs + '</p></body></html>']
---
+++ sara_cmt/cmt_server/setup.py
@@ -0,0 +1,93 @@
+#    This file is part of CMT, a Cluster Management Tool made at SURFsara.
+#    Copyright (C) 2012, 2013  Sil Westerveld, Ramon Bastiaans
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+from distutils.core import setup
+from cmt_server import __version__
+
+setup(
+    name = 'CMT',
+    version = __version__,
+    description = 'Cluster Management Tool',
+    url = 'http://oss.trac.sara.nl/cmt/',
+    author = 'CMT Development team',
+    author_email = 'cmt-users@lists.osd.sara.nl',
+    license = 'GPL',
+    long_description = 'CMT is a Cluster Management Tool originally created at SURFsara.',
+
+    platforms = [ 'linux-x86_64', 'linux-i386' ],
+
+    # see: http://pypi.python.org/pypi?:action=list_classifiers
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: System Administrators',
+        'License ::  OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.6',
+        'Topic :: Database :: Front-Ends',
+        'Topic :: System :: Clustering',
+        'Topic :: System :: System Administration',
+        'Topic :: Utilities',
+    ],
+
+    packages = [ 'cmt_server', 'cmt_server.apps', 'cmt_server.apps.api', 'cmt_server.apps.cluster', 'cmt_server.apps.cluster.templatetags' ],
+
+    install_requires = [
+        'djangorestframework>=2.3.6',
+        'Python>=2.6.0','Python<3.0',
+        'Django>=1.5.0','Django<1.6.0',
+        'IPy>=0.80',
+        'django_extensions>=1.1.1',
+        'psycopg2>=2.4.6',
+        'django-grappelli>=2.4.9','django-grappelli<2.5.0'
+        'django-smuggler',
+        'django_auth_ldap',
+        'django-debug-toolbar',
+        'south',
+        'feedparser'
+    ],
+
+    # Only way to get something installed with mode 755 ...
+    scripts = [ 'bin/cmt' ],
+
+    # data_files = ( target_dir, source_files )
+    # if target_dir is not absolute, will use site.sys.prefix
+    data_files = [
+
+        # config-files
+        ( 'etc/cmt', [
+            'etc/cmt.conf.sample',
+            'etc/logging.conf'
+        ]),
+
+        # templates
+        ( 'etc/cmt/templates', [
+            'templates/examples/README'
+        ]),
+
+        # examples of CMT-templates
+        ( 'etc/cmt/templates/examples', [
+            'templates/examples/simple_cnames.cmt',
+            'templates/examples/simple_hostnames.cmt',
+            'templates/examples/simple_dhcpd.conf.cmt',
+            'templates/examples/complex_dns.cmt'
+        ])
+    ]
+)
