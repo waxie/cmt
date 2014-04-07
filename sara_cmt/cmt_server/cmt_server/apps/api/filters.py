e09ce3198505061e9e30a543ffcc8a7e4878d927
Ramon Bastiaans <ramon.bastiaans@surfsara.nl>
2014-04-07 13:09:57 +0200

    cmt_server/cmt_server/apps/api/filters.py:
     * add all other hardwareunit filters and filter relation mappings

--- sara_cmt/cmt_server/cmt_server/apps/api/filters.py
+++ sara_cmt/cmt_server/cmt_server/apps/api/filters.py
@@ -4,15 +4,20 @@ from cmt_server.apps.cluster.models import HardwareUnit as Equipment
 
 class EquipmentFilter(django_filters.FilterSet):
 
-    cluster = django_filters.CharFilter(name="cluster__name")
-    rack = django_filters.CharFilter(name="rack__label")
+    cluster        = django_filters.CharFilter(name="cluster__name")
+    rack           = django_filters.CharFilter(name="rack__label")
+    network        = django_filters.CharFilter(name="network__name")
+    specifications = django_filters.CharFilter(name="specifications__name")
+    warranty       = django_filters.CharFilter(name="warranty__label")
+    seller         = django_filters.CharFilter(name="seller__name")
+    owner          = django_filters.CharFilter(name="owner__name")
 
     class Meta:
         model = Equipment
 
         fields =  [
-            'cluster', 'rack',
-            'serial_number', 'first_slot', 'label'
+            'cluster', 'rack', 'network', 'specifications', 'warranty', 'seller', 'owner',
+            'state', 'warranty_tag', 'serial_number', 'first_slot', 'label'
             ]
 
         order_by = 'label'
