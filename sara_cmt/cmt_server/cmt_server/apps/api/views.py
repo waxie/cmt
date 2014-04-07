5b96cc193b60698aeffb8233f4ef85ec5c367cac
Ramon Bastiaans <ramon.bastiaans@surfsara.nl>
2014-04-07 17:25:51 +0200

    cmt_server/cmt_server/apps/api/views.py:
     * filter queryset based on arguments containing __
     * this allows things like: read equipment --get label__startswith=r7
     * now CLI behaviour is the same as server "read" template tag:
       * first apply any queryset filters (if any) on queryset, i.e.: label__startwith or warranty__created_on
       * secondly, with resulting queryset: apply django-filter filters, i.e.: cluster=Lisa, rack=r7, label=r2n1, etc

--- sara_cmt/cmt_server/cmt_server/apps/api/views.py
+++ sara_cmt/cmt_server/cmt_server/apps/api/views.py
@@ -1,6 +1,7 @@
 # vim: set noai tabstop=4 shiftwidth=4 expandtab:
 
 import pprint
+import json
 
 ### Create your views here.
 ##
@@ -33,6 +34,9 @@ from django.utils.encoding import force_unicode
 from django.contrib.contenttypes.models import ContentType
 
 from django.template import TemplateSyntaxError
+
+from django.core.exceptions import FieldError
+from django.http import HttpResponse
 
 #from rest_framework_bulk import ListBulkCreateUpdateDestroyAPIView
 
@@ -101,6 +105,38 @@ class CMTViewSet(viewsets.ModelViewSet):
             'fields': fields
         }
 
+    def get_queryset(self):
+        """
+        Optionally filters the returned queryset based on query arguments
+
+        This allows things like this to work: label__startswith=r7
+        """
+        queryset = self.entity_object.objects.all()
+
+        q_filter_dict = { }
+
+        for attr, val in self.request.QUERY_PARAMS.items():
+            if not attr or not val:
+                continue
+
+            # only do this for queryset filters that contain __ (i.e.: label__startswith)
+            # all other filters should (and will) be handled by filter_class (django-filter)
+            if str(attr).find( '__' ) != -1:
+                q_filter_dict[ str(attr) ] = str(val)
+
+        # Perform that queryset filtering magic
+        if len(q_filter_dict) > 0:
+            try:
+                queryset = queryset.filter( **q_filter_dict )
+            except FieldError as details:
+                # someone is making a typo: but we can't return an error to client from here
+                # So they should learn to type and simply return to original queryset
+                queryset = queryset
+        else:
+            queryset = queryset
+
+        return queryset
+
 #####
 #
 # Viewsets based on the models in CMT. They define the view behavior.
@@ -108,83 +144,99 @@ class CMTViewSet(viewsets.ModelViewSet):
 #
 
 class ClusterViewSet(CMTViewSet):
+    entity_object = Cluster
     queryset = Cluster.objects.all()
     serializer_class = ClusterSerializer
     fields = ('url', 'name')
     filter_class = ClusterFilter
 
 class EquipmentViewSet(CMTViewSet):
+    entity_object = Equipment
     queryset = Equipment.objects.all()
     serializer_class = EquipmentSerializer
     filter_class = EquipmentFilter
 
 class RackViewSet(CMTViewSet):
+    entity_object = Rack
     queryset = Rack.objects.all()
     serializer_class = RackSerializer
     filter_class = RackFilter
 
 class RoomViewSet(CMTViewSet):
+    entity_object = Room
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
     filter_class = RoomFilter
 
 class AddressViewSet(CMTViewSet):
+    entity_object = Address
     queryset = Address.objects.all()
     serializer_class = AddressSerializer
     filter_class = AddressFilter
 
 class CountryViewSet(CMTViewSet):
+    entity_object = Country
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
     filter_class = CountryFilter
 
 # Network-related
 class InterfaceViewSet(CMTViewSet):
+    entity_object = Interface
     queryset = Interface.objects.all()
     serializer_class = InterfaceSerializer
     filter_class = InterfaceFilter
 
 class NetworkViewSet(CMTViewSet):
+    entity_object = Network
     queryset = Network.objects.all()
     serializer_class = NetworkSerializer
     filter_class = NetworkFilter
 
 class ConnectionViewSet(CMTViewSet):
+    entity_object = Connection
     queryset = Connection.objects.all()
     serializer_class = ConnectionSerializer
     filter_class = ConnectionFilter
 
 class CompanyViewSet(CMTViewSet):
+    entity_object = Company
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
     filter_class = CompanyFilter
 
 class TelephonenumberViewSet(CMTViewSet):
+    entity_object = Telephonenumber
     queryset = Telephonenumber.objects.all()
     serializer_class = TelephonenumberSerializer
     filter_class = TelephonenumberFilter
 
 class HardwareModelViewSet(CMTViewSet):
+    entity_object = HardwareModel
     queryset = HardwareModel.objects.all()
     serializer_class = HardwareModelSerializer
     filter_class = HardwareModelFilter
 
 class RoleViewSet(CMTViewSet):
+    entity_object = Role
     queryset = Role.objects.all()
     serializer_class = RoleSerializer
     filter_class = RoleFilter
 
 class InterfaceTypeViewSet(CMTViewSet):
+    entity_object = InterfaceType
     queryset = InterfaceType.objects.all()
     serializer_class = InterfaceTypeSerializer
     filter_class = InterfaceTypeFilter
 
 class WarrantyTypeViewSet(CMTViewSet):
+    entity_object = WarrantyType
     queryset = WarrantyType.objects.all()
     serializer_class = WarrantyTypeSerializer
     filter_class = WarrantyTypeFilter
 
 class WarrantyContractViewSet(CMTViewSet):
+    entity_object = WarrantyContract
     queryset = WarrantyContract.objects.all()
     serializer_class = WarrantyContractSerializer
     filter_class = WarrantyContractFilter
@@ -211,8 +263,6 @@ class TemplateView(APIView):
         from django.template import Context
         from django.template.loader import render_to_string
         from django.template import TemplateDoesNotExist
-        from django.http import HttpResponse
-        import json
 
         try:
             # Initialize a Context to render the template with
