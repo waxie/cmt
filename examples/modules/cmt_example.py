5f93ea5161bae28c8fcf252d4df743f9d551e2a7
Dennis Stam <dennis.stam@surfsara.nl>
2016-08-18 12:27:18 +0200

    Update cmt_example.py

--- examples/modules/cmt_example.py
+++ examples/modules/cmt_example.py
@@ -3,7 +3,7 @@
 ### export PYTHONPATH=<pwd_client_dir>
 import cmt_client
 
-client = Client()
+client = cmt_client.Client()
 
 json = client.request('read', 'equipment', ['rack=Z3A-H08', 'first_slot=17'], None)
 print json
